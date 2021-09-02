56b7415966f426258e07f7ec63cbd324d3fe12e9
Gun <2416580158@qq.com>
2021-09-02 16:33:05 +0800

    The first commit

---
+++ src/Stu.java
@@ -0,0 +1,12 @@
+public class Stu {
+    public class proctice9 {
+
+    }
+    class Student{
+        private int id;  //学号
+        private String name;    //姓名
+        private int age;    //年龄
+        private String address; //居住地
+
+    }
+}
